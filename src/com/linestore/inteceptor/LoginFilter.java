bf73e179c192132c60038c4c74de8f89716b8328
Liudel <18232450029@163.com>
2017-07-30 12:53:38 +0800

    登录检测
    登录检测

---
+++ src/com/linestore/inteceptor/LoginFilter.java
@@ -0,0 +1,54 @@
+package com.linestore.inteceptor;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;  
+	    HttpServletResponse res = (HttpServletResponse)response;  
+	    String path = req.getContextPath(); 
+	    String indexPath = req.getScheme()+"://"+req.getServerName()+":"+req.getServerPort()+path+ "/home/login.jsp"; 
+	    System.out.println(indexPath);
+	    if(req.getRequestURI().endsWith("login.jsp") 
+	    		|| req.getRequestURI().endsWith("register.jsp") 
+	    		|| req.getRequestURI().endsWith(".css") 
+	    		|| req.getRequestURI().endsWith(".js"))
+	    { 
+	      chain.doFilter(request, response);  
+	      return; 
+	    } 
+	      
+	    Object loginuser = req.getSession().getAttribute("user");  
+	    if(loginuser == null){ 
+	      res.sendRedirect(indexPath);
+	      return;  
+	    } 
+	    chain.doFilter(request, response);
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
