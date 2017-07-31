143c456fa40fcde178b4e4e1691a3cf21e7a2eeb
Liudel <18232450029@163.com>
2017-07-31 20:06:06 +0800

    BusTrading持久化类修改2
    BusTrading持久化类修改2

---
+++ src/com/linestore/vo/BusTrading.java
@@ -0,0 +1,81 @@
+package com.linestore.vo;
+
+import java.sql.Timestamp;
+
+/**
+ * BusTrading entity. @author MyEclipse Persistence Tools
+ */
+
+public class BusTrading implements java.io.Serializable {
+
+	// Fields
+
+	private String btaId;
+	private Business business;
+	private Integer btaType;
+	private Float btaMoney;
+	private Timestamp btaTime;
+
+	// Constructors
+
+	/** default constructor */
+	public BusTrading() {
+	}
+
+	/** minimal constructor */
+	public BusTrading(String btaId) {
+		this.btaId = btaId;
+	}
+
+	/** full constructor */
+	public BusTrading(String btaId, Business business, Integer btaType, Float btaMoney, Timestamp btaTime) {
+		this.btaId = btaId;
+		this.business = business;
+		this.btaType = btaType;
+		this.btaMoney = btaMoney;
+		this.btaTime = btaTime;
+	}
+
+	// Property accessors
+
+	public String getBtaId() {
+		return this.btaId;
+	}
+
+	public void setBtaId(String btaId) {
+		this.btaId = btaId;
+	}
+
+	public Business getBusiness() {
+		return this.business;
+	}
+
+	public void setBusiness(Business business) {
+		this.business = business;
+	}
+
+	public Integer getBtaType() {
+		return this.btaType;
+	}
+
+	public void setBtaType(Integer btaType) {
+		this.btaType = btaType;
+	}
+
+	public Float getBtaMoney() {
+		return this.btaMoney;
+	}
+
+	public void setBtaMoney(Float btaMoney) {
+		this.btaMoney = btaMoney;
+	}
+
+	public Timestamp getBtaTime() {
+		return this.btaTime;
+	}
+
+	public void setBtaTime(Timestamp btaTime) {
+		this.btaTime = btaTime;
+	}
+
+}
